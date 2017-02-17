7df1996198c7da957547161210da005548bfd257
joshuajhun <joshuajhun@gmail.com>
2017-02-17 10:29:21 -0700

    fixed my shitty tests

--- spec-harness/complete-me-test.js
+++ spec-harness/complete-me-test.js
@@ -1,4 +1,4 @@
-import CompleteMe from '../../complete-me/tdd/scripts/CompleteMe'
+import Trie from '../../complete-me/scripts/trie'
 import { assert }  from 'chai'
 const fs = require('fs')
 
@@ -6,7 +6,7 @@ describe('spec harness', () => {
   var complete
 
   beforeEach(() => {
-    complete = new CompleteMe
+    complete = new Trie
   });
 
   it('should have a count of 0', () => {
@@ -19,12 +19,12 @@ describe('spec harness', () => {
   });
 
   it('should have a populate function that lets me add multiple words', () => {
-    complete.populate('pizza\ndog\ncat')
+    complete.populate('pizza\ndog\ncat'.split('\n'))
     assert.equal(complete.count, 3)
   });
 
   it('should allow me to populate an array of words', () => {
-    complete.populate(["shaka", "aardvark", "zombies", "a", "xylophones"].join('\n'))
+    complete.populate(["shaka", "aardvark", "zombies", "a", "xylophones"])
 
     assert.equal(complete.count, 5)
     assert.deepEqual(["shaka"], complete.suggest("s"))
@@ -37,20 +37,21 @@ describe('spec harness', () => {
 
   it('should read in a small data set', () => {
     let text = fs.readFileSync('./spec-harness/medium.txt','utf-8').trim().split('\n')
+
     complete.populate(text);
 
-    assert.equal(text.length, complete.length)
+    assert.equal(text.length, complete.count)
   });
 
   it('should suggest the correct words', () => {
     let text = fs.readFileSync('./spec-harness/medium.txt','utf-8').trim().split('\n')
     complete.populate(text);
 
-    assert.deepEqual(["williwaw", "wizardly"], complete.suggest('w').sort())
+    assert.deepEqual(["williwaw", "wizardly"], complete.suggest('wi').sort())
 
-    complete.select('wi', 'wizardly')
+    complete.select('wizardly')
 
-    assert.deepEqual(["wizardly", "williwaw"], complete.suggest('w'))
+    assert.deepEqual(["wizardly", "williwaw"], complete.suggest('wi'))
   });
 
   it('should load in a large data set', ()=> {
@@ -59,9 +60,9 @@ describe('spec harness', () => {
     complete.populate(text);
 
     assert.deepEqual(["doggerel", "doggereler", "doggerelism", "doggerelist",
-                 "doggerelize", "doggerelizer"], complete.suggest("doggerel").sort)
+                 "doggerelize", "doggerelizer"], complete.suggest("doggerel"))
 
-    complete.select('doggerel', 'doggerelist')
+    complete.select( 'doggerelist')
     assert.deepEqual('doggerelist', complete.suggest('doggerel')[0])
 
   });
